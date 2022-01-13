77c70a1570d0b7cc2f8a9d032a91aad71a1a6cfe
evant2 <thompsone6@students.westerntc.edu>
2022-01-12 18:01:21 -0600

    Party Invites part 1

---
+++ PartyInvites/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            string greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+            ViewBag.Greeting = greeting;
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RsvpForm(Guest guest)
+        {
+            Repository.AddResponse(guest);
+            return View("Thanks", guest);
+        }
+
+        public ActionResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r=>r.WillAttend==true));
+        }
+    }
+}

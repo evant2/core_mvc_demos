71308e96391d323f015b9af30201e4a281e1e220
evant2 <thompsone6@students.westerntc.edu>
2022-02-08 09:41:59 -0600

    finished chapter 5 video

---
+++ Razer/Razor/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*
+        public IActionResult Index()
+        {
+            Product prod = new Product()
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+
+            ViewBag.StockLevel = 2;
+
+            return View(prod);
+        }
+        */
+        public IActionResult Index()
+        {
+            Product[] prodArr = new Product[]
+            {
+                new Product(){Name = "Kayak", Price = 275M},
+                new Product(){Name = "Lifejacket", Price = 48.95m},
+                new Product(){Name = "Soccer Ball", Price = 19.50m},
+                new Product(){Name = "Corner Flag", Price = 34.95m}
+            };
+
+            ViewBag.StockLevel = 2;
+
+            return View(prodArr);
+        }
+    }
+}
